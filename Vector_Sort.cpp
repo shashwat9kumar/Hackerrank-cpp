cea316dee2fec08d172aa2c42b984fe5741e9f0b
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2019-12-30 14:48:05 +0530

    Update Vector_Sort.cpp

--- Vector_Sort.cpp
+++ Vector_Sort.cpp
@@ -56,3 +56,17 @@ Sample Output
 #include <iostream>
 #include <algorithm>
 using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    
+    int n;
+    cin>>n;
+    vector<int> arr;
+    for(int i=0;i<n;i++)
+    {
+        int val;
+        cin>>val;
+        arr.push_back(val);
+    }
